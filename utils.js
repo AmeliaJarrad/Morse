4485ccfe73996b51b0176f0eeb2831b3ef5ffd8b
AmeliaJarrad <amelia.jarrad@gmail.com>
2025-04-05 17:00:07 +1100

    initial branch commit

--- utils.js
+++ utils.js
@@ -28,7 +28,6 @@ const alphamorse =
 }
 
 
-
 export function getAlphFromMorse(object, value) {
     
     return Object.keys(object).find(key => 
@@ -38,7 +37,8 @@ export function getAlphFromMorse(object, value) {
 const a2m = getAlphFromMorse(alphamorse, "--..");
 console.log(a2m);
 
-
+//couldn't get the mirror of getAlphFromMorse to work, the .find only would return first 
+//character in the morse result, so have used for loop
 
 export function getMorseFromAlpha(object, key) {
     
@@ -51,5 +51,17 @@ export function getMorseFromAlpha(object, key) {
     }
 }
 
+
+
 const m2a = getMorseFromAlpha(alphamorse, "Q");
 console.log(m2a);
+
+// need to create some function that succesfully takes user input, if input is alpha - use
+// .split to firstly split each word using delimiter " " (single space) and store each word 
+// into an array, then .split each word in the arrray to take each letter in a word, then 
+// return the morse character for each letter in the word, then use .join for the return with
+//  each morse character delimited with " "(single space). 
+
+const userInput = document.querySelector("#input");
+const usersWords = [];
+
