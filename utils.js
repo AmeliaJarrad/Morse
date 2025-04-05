205f983ae264b8d79fa640fb123510933d459c0a
AmeliaJarrad <amelia.jarrad@gmail.com>
2025-04-05 17:52:59 +1100

    todo identified

--- utils.js
+++ utils.js
@@ -58,10 +58,21 @@ console.log(m2a);
 
 // need to create some function that succesfully takes user input, if input is alpha - use
 // .split to firstly split each word using delimiter " " (single space) and store each word 
-// into an array, then .split each word in the arrray to take each letter in a word, then 
+// into an array, then .split each word in the array to take each letter in a word, then 
 // return the morse character for each letter in the word, then use .join for the return with
-//  each morse character delimited with " "(single space). 
+// each morse character delimited with " "(single space) for each letter, and "  " (double
+// space) between each word . 
 
 const userInput = document.querySelector("#input");
-const usersWords = [];
+const usersAWords = [];
 
+
+
+// need to create some function that succesfully takes user input, if input is morse - use
+// .split to firstly split each morseword using delimiter "  " (double space) and store each 
+// morseword into an array, then .split each morseword in the array using single space " " as
+// the delimiter to separate each morseletter in a word, then return the alpha letter for each 
+// morseletter in the word, then use .join for the return with each morse character
+//  with ""(no space) between each letter, and " " (single space) between each word .
+
+const usersMWords = [];
