35448f3ddfc363911158208201a2c3a0872c6b9d
AmeliaJarrad <amelia.jarrad@gmail.com>
2025-04-06 13:45:08 +1000

    got it working huzzuh

--- script.js
+++ script.js
@@ -42,23 +42,97 @@ const myMorse = Object.values(alphamorse)
 
 import { getAlphFromMorse } from "./utils.js";
 import { getMorseFromAlpha } from "./utils.js";
+import { lettersOrMorseOnly } from "./utils.js";
 
 
 
 //possibly above can be redone with a .map and/or .filter
 
 const form = document.querySelector("form");
+const output = document.getElementById("output")
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
     console.log("form submitted")
     const userInput = document.querySelector("#input")
     const userText = userInput.value.toUpperCase()
-    console.log(userText)
-    const a2m = getAlphFromMorse(alphamorse, userText)
-    const m2a = getMorseFromAlpha(alphamorse, userText);
-    console.log(m2a, a2m, "is the translation")
+    console.log("User has inputted " + userText)
 
-});
+//here is where I figure out if its words or morse being input by user
+    
+    let val = userText
+    console.log(val + " is this val")
+    let isAlphabetic = /^[a-zA-Z ]+$/.test(val);
+    console.log(isAlphabetic)
 
-//currently this works for single character inputs only 
+    if (isAlphabetic) {
+        console.log("Receiving text input")
+        const words = val.split(" ");
+        console.log("attempting split " + words + " this is now an Array")
+        console.log(words)
+    // now I have the array called words, I need to iterate over it, can't split coz array not string
+        const letters = words.map((x) => x.split(""))
+        console.log(letters)
+        let convertedArray = []
+        
+        for (let i = 0; i < letters.length; i++) {
+           convertedArray[i] = [];
+            for (let j = 0; j < letters[i].length; j++) {
+                convertedArray[i][j] = getMorseFromAlpha(alphamorse, letters[i][j])
+            }
+            convertedArray[i] = convertedArray[i].join(" ")
+        }
+        console.log(convertedArray)
+        let presentString = ""
+        for (let i = 0; i < convertedArray.length; i++) {
+            presentString = presentString + convertedArray[i] + "    "
+        }
+        console.log(presentString)
+        createTextBox("div", " from English to morse is " + presentString, output, ["output"])
+        // try {
+        //     const m2a = getMorseFromAlpha(alphamorse, userText)
+        //     console.log("Translation from English is " + m2a )
+        //     // const m2al = getMorseFromAlpha(alphamorse, letters)
+        //     // console.log("letters " + m2al)
+        //     createTextBox("div", "Translation from English to morse is " + m2a, output,  )
+        // } catch (e) {
+        //     ("ERROR WAS THROWN " + e)
+        // }
+
+    }
+
+    if (!isAlphabetic) {
+        console.log("Receiving morse input")
+        const morseWords = val.split("    ");
+        console.log("attempting split " + morseWords + " this is now an Array")
+        console.log(morseWords)
+        const morseLetters = morseWords.map((x) => x.split(" "))
+        console.log(morseLetters)
+        let convertedMorseArray = []
+
+        for (let i = 0; i < morseLetters.length; i++) {
+            convertedMorseArray[i] = [];
+             for (let j = 0; j < morseLetters[i].length; j++) {
+                 convertedMorseArray[i][j] = getAlphFromMorse(alphamorse, morseLetters[i][j])
+             }
+             convertedMorseArray[i] = convertedMorseArray[i].join("")
+         }
+         console.log(convertedMorseArray)
+         let presentMorseString = ""
+         for (let i = 0; i < convertedMorseArray.length; i++) {
+             presentMorseString = presentMorseString + convertedMorseArray[i] + " "
+         }
+         console.log(presentMorseString)
+         createTextBox("div", " from morse to English is " + presentMorseString.toLowerCase(), output, ["output"])
+    //      try {
+    //         const a2m = getAlphFromMorse(alphamorse, userText)
+    //         console.log("Translation from Morse is " + a2m )
+    //         createTextBox("div", "Translation from morse to English is " + a2m, output,  )
+    //     } catch (e) {
+    //         ("ERROR WAS THROWN " + e)
+    //     }
+       
+
+    }
+})
+
--- utils.js
+++ utils.js
@@ -34,15 +34,15 @@ export function getAlphFromMorse(object, value) {
         object[key] === value);
 }
 
-const a2m = getAlphFromMorse(alphamorse, "--..");
-console.log(a2m);
+// const a2m = getAlphFromMorse(alphamorse, "--..");
+// console.log(a2m);
 
 //couldn't get the mirror of getAlphFromMorse to work, the .find only would return first 
 //character in the morse result, so have used for loop
 
 export function getMorseFromAlpha(object, key) {
     
-    for (let i =0; i< Object.values(object).length; i++)
+    for (let i = 0; i < Object.values(object).length; i++)
     {
         if(Object.keys(object)[i] === key)
         {
@@ -53,26 +53,11 @@ export function getMorseFromAlpha(object, key) {
 
 
 
-const m2a = getMorseFromAlpha(alphamorse, "Q");
-console.log(m2a);
-
-// need to create some function that succesfully takes user input, if input is alpha - use
-// .split to firstly split each word using delimiter " " (single space) and store each word 
-// into an array, then .split each word in the array to take each letter in a word, then 
-// return the morse character for each letter in the word, then use .join for the return with
-// each morse character delimited with " "(single space) for each letter, and "  " (double
-// space) between each word . 
-
-const userInput = document.querySelector("#input");
-const usersAWords = [];
+// const m2a = getMorseFromAlpha(alphamorse, "Q");
+// console.log(m2a);
 
 
-
-// need to create some function that succesfully takes user input, if input is morse - use
-// .split to firstly split each morseword using delimiter "  " (double space) and store each 
-// morseword into an array, then .split each morseword in the array using single space " " as
-// the delimiter to separate each morseletter in a word, then return the alpha letter for each 
-// morseletter in the word, then use .join for the return with each morse character
-//  with ""(no space) between each letter, and " " (single space) between each word .
-
-const usersMWords = [];
+export function lettersOrMorseOnly(input) {
+    var regex = /[^a-z\s-.]/gi;
+    input.value = input.value.replace(regex, "")
+}
